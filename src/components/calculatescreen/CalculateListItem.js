9752df7632d504b05dd0bcd4561c0ad53c2a51d9
Filippov Anton <moriarch@outlook.com>
2022-05-19 21:42:36 +0300

    Touchebla opacity

--- src/components/calculatescreen/CalculateListItem.js
+++ src/components/calculatescreen/CalculateListItem.js
@@ -1,43 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {colors, font, h1} from '../../constants/theme';
-import {Text, StyleSheet, View} from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
 export default function CalculateListItem({data}) {
   return (
-    <View style={styles.body}>
-       <View style={styles.bodyChar}><Text style={styles.char}>{data.name[0]}</Text></View>
-      <Text style={styles.text}>{data.name}</Text>
-    </View>
+    <TouchableOpacity>
+      <View style={styles.body}>
+        <View style={styles.bodyChar}>
+          <Text style={styles.char}>{data.name[0]}</Text>
+        </View>
+        <Text style={styles.text}>{data.name}</Text>
+      </View>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   body: {
-    padding:10,
-    backgroundColor:'#191A1D',
-    marginBottom:15,
-    borderRadius:15,
-    fontFamily:font.normal,
-    flexDirection:'row',
-    alignItems:'center',
-    
+    padding: 10,
+    backgroundColor: '#191A1D',
+    marginBottom: 15,
+    borderRadius: 15,
+    fontFamily: font.normal,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   text: {
-    color:'white',
-    paddingLeft:15
+    color: 'white',
+    paddingLeft: 15,
   },
-  bodyChar:{
-    
-    justifyContent:'center',
-    alignItems:'center',
-    borderRadius:100,
-    width:50,
-    height:50,
-    backgroundColor:colors.primary,
+  bodyChar: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+    width: 50,
+    height: 50,
+    backgroundColor: colors.primary,
   },
-  char:{
-    color:'white',
-    fontFamily:font.bold,
-    fontSize:25,
-    padding:10
-  }
+  char: {
+    color: 'white',
+    fontFamily: font.bold,
+    fontSize: 25,
+    padding: 10,
+  },
 });
